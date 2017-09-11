0f0f33c9ca93de5ea4e74328f61d429dbcb6831d
sruthyv <vsruthys@gmail.com>
2017-09-11 18:14:09 +0530

    file unzipped change varaibles

--- csv_unzip.rb
+++ csv_unzip.rb
@@ -8,21 +8,21 @@ class CsvUnzip
             f_path = File.join(destination, f.name)
             FileUtils.mkdir_p(File.dirname(f_path))
             f.extract(f_path)unless File.exist?(f_path)
-            @a=f
+            @url=f
          end
       end
-      a="#{@a}"
+      url="#{@url}"
       i=1
-      $ar=[]
+      $arraysort=[]
       CSV.foreach(a) do |row| 
-         $ar << row[1]
+         $arraysort << row[1]
          i=i+1
          if (i==1000) 
             break
          end
       end
       # puts ($ar)
-      File.open("tempunzip", 'w') { |file| file.puts($ar) }
+      File.open("tempunzip", 'w') { |file| file.puts($arraysort) }
    end        
 end
 
