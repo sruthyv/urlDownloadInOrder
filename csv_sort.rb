72c4252b02d0e6f0b00ec5b93e85ff23591933a2
sruthyv <vsruthys@gmail.com>
2017-09-11 17:45:25 +0530

    csv file sorted

---
+++ csv_sort.rb
@@ -0,0 +1,29 @@
+require 'csv'
+require 'rubygems'
+require 'zip'
+
+class CsvSort
+    puts "SORTED LIST:::::::::::::::::::"
+    def mergesort(list)
+        # puts list
+        return list if list.size <= 1
+        mid   = list.size / 2
+        left  = list[0, mid]
+        right = list[mid, list.size]
+        merge(mergesort(left), mergesort(right))
+    end
+  
+    def merge(left, right)
+        sorted = []
+        until left.empty? || right.empty?
+            if left.first <= right.first
+                sorted << left.shift
+            else
+                sorted << right.shift
+            end
+        end
+        @sortedlist=sorted.concat(left).concat(right)
+    end
+end
+
+
