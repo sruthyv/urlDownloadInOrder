1e1ab897a5d698a5b6a65f29565fc449ef065f93
sruthyv <vsruthys@gmail.com>
2017-09-11 17:46:41 +0530

    test that csv file downloaded

---
+++ csv_download_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun" 
+require_relative "csv_download"
+class CsvDownloadTest < Minitest::Test
+    def setup
+        @download=CsvDownload.new
+        @download.csv_download
+    end                        
+    def test_is_file_downloaded  
+        assert File.exist?("/home/user/test1/top.zip"),true
+    end
+end
+
+
+
