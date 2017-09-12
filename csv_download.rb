dbb517ac4dec31c32177372a7e65d488a66b2532
sruthyv <vsruthys@gmail.com>
2017-09-12 13:04:26 +0530

    csv file downloaded

--- csv_download.rb
+++ csv_download.rb
@@ -5,7 +5,7 @@ class CsvDownload
         Net::HTTP.start("s3.amazonaws.com") do |http|
             content = http.get("http://s3.amazonaws.com/alexa-static/top-1m.csv.zip")
             @write=WriteFile.new
-            @write.write_file("csvzipfile.zip",content)
+            @write.write_file("csvZipFile.zip",content)
         puts"csv file downloaded"
         end
     end
