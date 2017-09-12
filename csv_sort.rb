634a130a8e4993545fd77d3d83a2765f0e838af1
sruthyv <vsruthys@gmail.com>
2017-09-12 11:56:06 +0530

    csv file sorted ,removed global varaibles

--- csv_sort.rb
+++ csv_sort.rb
@@ -1,6 +1,9 @@
 require 'csv'
 require 'rubygems'
 require 'zip'
+require_relative 'csv_download'
+require_relative 'csv_unzip'
+require_relative 'write_file'
 
 class CsvSort
     puts "SORTED LIST:::::::::::::::::::"
@@ -24,7 +27,24 @@ class CsvSort
         end
         @sortedlist=sorted.concat(left).concat(right)
     end
+    def returnSortedList
+        return @sortedlist
+    end
 end
+@downloadFile=CsvDownload.new
+@downloadFile.csv_download
+
+@unzipList=CsvUnzip.new
+@unzipList.csv_unzip("top.zip", "/home/user/urlDownloadInOrder")
+@arraySortList=@unzipList.returnUnzipList
+
 @sortList=CsvSort.new
-@sortList.mergesort($arraysort)
+@sortList.mergesort(@arraySortList)
+@resultSort=@sortList.returnSortedList
+
+@write=WriteFile.new
+@write.write_file("sortingList",@resultSort)
+
+
+
 
