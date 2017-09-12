0564156fafe7c346085b8050d842cc2740731d4c
sruthyv <vsruthys@gmail.com>
2017-09-12 09:45:21 +0530

    test for unzip csv file

--- csv_unzip_test.rb
+++ csv_unzip_test.rb
@@ -3,10 +3,10 @@ require_relative "csv_unzip"
 class CsvUnzipTest < Minitest::Test
     def setup
         @unzipfile=CsvUnzip.new
-        @unzipfile.csv_unzip("top.zip", "/home/user/test1")
+        @unzipfile.csv_unzip("top.zip", "/home/user/urlDownloadInOrder")
 
     end 
     def test_is_file_unzipped  
-        assert File.exist?("/home/user/test1/top-1m.csv"),true
+        assert File.exist?("/home/user/urlDownloadInOrder/top-1m.csv"),true
     end
 end
